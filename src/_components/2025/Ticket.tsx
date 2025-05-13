1960225453dd4158a709be1c3f7c2c2f7178b4c2
Seungmin Lee <rfrost77@gmail.com>
2025-05-14 00:37:47 +0900

    add ticket and faq

--- src/_components/2025/Ticket.tsx
+++ src/_components/2025/Ticket.tsx
@@ -8,9 +8,15 @@ export function Ticket() {
           <h2 className="mb-4 text-sm font-medium text-[#5180FF] md:text-2xl">Ticket</h2>
           <h3 className="text-2xl font-bold text-white md:text-5xl xl:text-6xl">행사장 입장부터, 한정 굿즈까지</h3>
         </div>
-        <div className="mx-auto max-w-[720px] rounded-[5px] bg-[#0F0F0F] px-4 py-9 text-center md:py-[46px]">
-          <p className="mb-2.5 text-sm font-semibold text-[#5180FF] md:text-2xl">일반</p>
-          <p className="text-2xl font-bold text-white md:text-4xl">59,000원</p>
+        <div className="mx-auto grid w-full max-w-[720px] grid-cols-2 gap-3 md:gap-5">
+          <div className="rounded-[5px] bg-[#0F0F0F] px-4 py-9 text-center md:py-[46px]">
+            <p className="mb-2.5 text-sm font-semibold text-[#5180FF] md:text-2xl">일반</p>
+            <p className="text-2xl font-bold text-white md:text-4xl">59,000원</p>
+          </div>
+          <div className="rounded-[5px] bg-[#0F0F0F] px-4 py-9 text-center md:py-[46px]">
+            <p className="mb-2.5 text-sm font-semibold text-[#5180FF] md:text-2xl">개인후원</p>
+            <p className="text-2xl font-bold text-white md:text-4xl">150,000원</p>
+          </div>
         </div>
         <div className="mt-[26px] flex justify-center md:mt-[72px]">
           <Link
