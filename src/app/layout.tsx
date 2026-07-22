10120fc175f55e5d27d8bce5e98c9660e50cc8be
Seungmin Lee <rfrost77@gmail.com>
2026-07-22 17:36:21 +0900

    fix icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "../_styles/globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://droidknights.dev"),
+  icons: {
+    shortcut: "/favicon.ico",
+    icon: { url: "/2026/favicon48.png", sizes: "48x48", type: "image/png" },
+    apple: { url: "/2026/favicon180.png", sizes: "180x180", type: "image/png" },
+  },
   openGraph: {
     images: "/2026/opengraph-image.png",
   },
